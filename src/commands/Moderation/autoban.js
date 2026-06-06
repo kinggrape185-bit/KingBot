84db583727c0cf3c34b55c9a0b629c270ded79df
kinggrape185-bit <kinggrape185@gmail.com>
2026-06-06 14:17:36 +0100

    Update autoban.js

--- src/commands/Moderation/autoban.js
+++ src/commands/Moderation/autoban.js
@@ -1,1 +1,24 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('autoban')
+        .setDescription('Enable autoban for a channel')
+        .addChannelOption(option =>
+            option.setName('channel')
+                .setDescription('Channel to protect')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+    async execute(interaction) {
+        const channel = interaction.options.getChannel('channel');
+
+        interaction.client.autobanChannels ??= new Set();
+        interaction.client.autobanChannels.add(channel.id);
+
+        await interaction.reply({
+            content: `AutoBan enabled for ${channel}.`,
+            ephemeral: true
+        });
+    }
+};
